480d929c56c71f9d9b2a0b0393284848ad0d6f79
BarbaraKostsina <107065349+BarbaraKostsina@users.noreply.github.com>
2022-08-02 14:07:39 +0300

    Add files via upload

---
+++ card.js
@@ -0,0 +1,48 @@
+let card = [{
+    photo: "sketchbook.jpg",
+    name: "Скетчбук акварельный",
+    brandName: "Malevich",
+    price: 20.55,
+    currency: "руб.", 
+},
+{
+    photo: "markers.png",
+    name: "Маркеры для рисования",
+    brandName: "Touch Raven",
+    price: 35.04,
+    currency: "руб.",
+},
+{ 
+    photo: "fabercastell.jpg",
+    name: "Карандаши для рисования",
+    brandName: "Faber Castell",
+    price: 100.95,
+    currency: "руб.",
+}
+]
+    
+
+   let s = "";
+
+    for (i=0; i<card.length; i++){
+  
+    s += 
+    `<div class="cards">
+            <div class="prodCard">
+                <div class="prodPhoto"><img src="images/${card[i].photo}" alt=""></div><br>
+                <div class="title1">${card[i].name}
+                    <div class="title2">${card[i].brandName}</div>
+                </div>
+            <div class="prodPrice">${card[i].price} ${card[i].currency}</div>
+            </div>
+        <br>
+    </div>`;
+
+    }
+
+    myCard.innerHTML = s;
+
+
+   
+
+  
